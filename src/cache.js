096a1a0e5d5ab840240f11f237d2aa5176e42caf
Moritz Schmitz von Hülst <msc@marketlogicsoftware.com>
2018-10-05 16:47:25 +0200

    implement status check

--- src/cache.js
+++ src/cache.js
@@ -1,3 +1,32 @@
+const cache = {};
+
+/**
+ * Returns the status of a promise.
+ *  1 - pending
+ *  2 - fulfilled
+ *  3 - rejected
+ *
+ * @param promise
+ * @returns {Promise<number>}
+ */
+const status = (promise) => {
+  const immediate = Promise.resolve(1);
+  return Promise.race([promise, immediate]).then(v => (v === 1 ? 1 : 2), () => 3);
+};
+
 export default (key, promise) => {
-  return promise;
+  const p = cache[key];
+
+  if (!p) {
+    cache[key] = promise;
+    return p;
+  }
+
+  return status(p).then(v => {
+    if (v === 1) {
+      return p;
+    }
+    cache[key] = promise;
+    return promise;
+  });
 };
